bdde57530731b0cbcb97c6c8be556f75e087f004
Frijaaay <jr.cortezjay@gmail.com>
2025-10-01 19:59:34 +0800

    artisan published cors

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,13 +13,15 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
+
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->alias([
             'jwt' => JwtMiddleware::class,
             // 'cors' => CorsHeaders::class
         ]);
-        $middleware->append(CorsHeaders::class);
+        // $middleware->append(CorsHeaders::class);
     })
+
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
