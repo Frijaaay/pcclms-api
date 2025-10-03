57d74432749b4d958f22f7ec58ba4f7e6d6641fd
Frijaaay <jr.cortezjay@gmail.com>
2025-10-04 04:27:39 +0800

    Update helper trait

--- app/Traits/ApiResponseTrait.php
+++ app/Traits/ApiResponseTrait.php
@@ -7,34 +7,78 @@ use Illuminate\Http\JsonResponse;
 trait ApiResponseTrait
 {
     /**
-     * Summary of handleSuccessResponse
-     * @param mixed $data data that goes into the content body
-     * @param string $message message included in the response
-     * @param int $code http response code
-     * @return JsonResponse
+     * Returns a standardized JSON success response.
+     * @param mixed $data Data that goes into the content body.
+     * @param string $message Message included in the response.
+     * @param int $code HTTP response code.
      */
-    protected function handleSuccessResponse(mixed $data = null, string $message = 'Success', int $code = 200): JsonResponse
+    protected function handleSuccessResponse(
+        mixed $data = null,
+        string $message = 'Operation completed successfully.',
+        int $code = 200): JsonResponse
     {
+        if (!$data) {
+            return response()->json([
+                'status' => 'success',
+                'message' => $message,
+            ], $code);
+        }
+
         return response()->json([
+            'status' => 'success',
             'message' => $message,
-            'content' => $data
+            'data' => $data
         ], $code);
     }
 
     /**
-     * Summary of handleErrorResponse
+     * Returns a standardized JSON error response.
      * @param string $message error message included in the response
      * @param int $code http response code
      * @param mixed $errors errors
      * @return JsonResponse
      */
-    protected function handleErrorResponse(string $message = 'Something went wrong );', int $code = 400): JsonResponse
+    protected function handleErrorResponse(string $message = 'An error occured.', mixed $errors, int $code = 400): JsonResponse
     {
         return response()->json([
-            'error' => [
-                'code' => $code,
-                'message' => $message,
-            ]
+            'status' => 'failed',
+            'code' => $code,
+            'message' => $message,
+            'errors' => $errors
         ], $code);
     }
+
+    /**
+     * Returns a standardized JSON with cookie response
+     * @param mixed $data
+     * @param mixed $cookie
+     * @param string $message
+     * @param int $code
+     * @return JsonResponse
+     */
+    protected function handleWithCookieResponse(
+        mixed $data,
+        mixed $cookie,
+        string $message = 'Operation completed successfully.',
+        int $code = 200): JsonResponse
+        {
+            return response()->json([
+                'status' => 'success',
+                'message' => $message,
+                'data' => $data
+            ], $code)
+            ->withCookie(
+                cookie(
+                    name: $cookie['name'],
+                    value: $cookie['value'],
+                    minutes: $cookie['expiry'],
+                    path: '/api/v1/auth',
+                    domain: null,
+                    secure: env('SESSION_SECURE_COOKIE', app()->environment('production')),
+                    httpOnly: true,
+                    raw: false,
+                    sameSite: 'strict'
+                )
+        );
+        }
 }
