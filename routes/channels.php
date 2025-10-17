24134693c528dc22af7778b41c51c848152c4793
Frijaaay <jr.cortezjay@gmail.com>
2025-10-17 16:18:32 +0800

    Re: Websocket

--- routes/channels.php
+++ routes/channels.php
@@ -1,7 +1,16 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Support\Facades\Broadcast;
 
-Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
-    return (int) $user->id === (int) $id;
+// Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
+//     return (int) $user->id === (int) $id;
+// });
+
+// Broadcast::channel('user.{id}', function ($user, $id) {
+//     return (int)$user->id === (int)$id;
+// });
+
+Broadcast::channel('users', function (User $user) {
+    return true;
 });
